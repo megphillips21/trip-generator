f29b58203e9cfbd49bf7935d24451a6e6e78dc17
Megan Phillips <megphillips21@gmail.com>
2022-05-19 16:11:14 -0700

    created lists for each city and transportation

---
+++ main.py
@@ -0,0 +1,22 @@
+destinations_list = ["Mammoth","Bishop","Lone Pine","Bridgeport", "Lee Vining"]
+transporations_list = ["Car","Tour Bus","Horse", "Town Trolley", "Bicycle"]
+
+# Mammoth lists of Resturants and Attractions
+mammoth_rests_list = ["Robertos","Burgers","Giovanni's","The Stove","The Yodler"]
+mammoth_attracts_list = ["Go Skiing", "Visit Hot Springs", "Look for Bears", "Go for a Hike", "Play golf"]
+
+# Bishop lists of Resturants and Attractions
+bishop_rests_list = ["Whiskey Creek", "Yamitani", "Salsa's", "El Pollo Loco", "Schat's Bakery"]
+bishop_attracts_list= ["Float the River", "Go Rock Climbing", "Go Fishing", "Horsebackriding", "Have an Offroad Adventure"]
+
+# Lone Pine lists of Resturants and Attractions
+lone_rests_list = ["Merry-go-Round", "Jersey Mikes", "Still Life Cafe", "Coppertop BBQ", "Two Brothers Pizza"]
+lone_attracts_list= ["Climb Mt. Whitney", "Walk along the John Muir Trail", "Go Camping", "Visit Death Valley", "Tour Scotty's Castle"]
+
+# Bridgeport lists of Resturants and Attractions
+bridgeport_rests_list = ["Virginia Creek", "Rhino's Bar and Grill", "Bridgeport Cafe", "Growler's Cafe"]
+bridgeport_attacts_list = ["Visit Bodie Ghost Town", "Fly fish on the River", "Go Boating on the lake", "Look at the Cows"]
+
+# Lee Vining lists of Resturants and Attractions
+leevining_rests_list = ["Mono Cone", "Mobile Mart", "Mono Inn", "Tioga Resort", "Brine Shrimp Factory"]
+leevining_attracts_list = ["Mono Lake", "Yosemite National Park", "Inyo Craters", "Visit the Sand Tuffas", "Double Eagle Spa"]
